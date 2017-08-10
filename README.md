54b75acd5279eee0efbb3487eddb9f54db1fde7b
Oleg G.Kapranov <lugatex@yahoo.com>
2017-08-10 17:55:05 +0300

    generated new version

---
+++ README.md
@@ -0,0 +1,11 @@
+# Rephink
+
+**Connecting to RethinkDB with Elixir**
+
+> Preparation
+
+```
+mix new rephink
+```
+
+### 10 Aug 2017 Oleg G.Kapranov
---
+++ lib/rephink.ex
@@ -0,0 +1,18 @@
+defmodule Rephink do
+  @moduledoc """
+  Documentation for Rephink.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Rephink.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
