41e2d140607ec155f2fce37ee790812ac7f9adcd
Oleg G.Kapranov <lugatex@yahoo.com>
2017-08-17 13:55:52 +0300

    added alt version seed

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -1,9 +1,40 @@
 alias Rephink.Repo
 alias Rephink.Posts.Post
 
-for _ <- 1..15 do
-  Repo.insert!(%Post{
-    title: Faker.Lorem.word,
-    content: Faker.Lorem.sentence
-  })
+# version 1
+initial_posts = [
+  [
+    {:title, Faker.Lorem.word},
+    {:content, Faker.Lorem.sentence}
+  ],
+  [
+    {:title, Faker.Lorem.word},
+    {:content, Faker.Lorem.sentence}
+
+  ],
+  [
+    {:title, Faker.Lorem.word},
+    {:content, Faker.Lorem.sentence}
+  ],
+  [
+    {:title, Faker.Lorem.word},
+    {:content, Faker.Lorem.sentence}
+  ]
+]
+
+for initial_post <- initial_posts do
+  Repo.insert!(
+    %Post{
+      title: initial_post[:title],
+      content: initial_post[:content]
+    }
+  )
 end
+
+# version 2
+#for _ <- 1..15 do
+#  Repo.insert!(%Post{
+#    title: Faker.Lorem.word,
+#    content: Faker.Lorem.sentence
+#  })
+#end
